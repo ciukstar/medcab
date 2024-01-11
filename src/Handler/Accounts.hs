21e1cb66d6af9d47bc830535a0ed43a093d037fc
ciukstar <ciukstar@yahoo.com>
2024-01-11 05:20:46 +0300

    Edit profile

--- src/Handler/Accounts.hs
+++ src/Handler/Accounts.hs
@@ -11,51 +11,76 @@ module Handler.Accounts
   , postAccountR
   ) where
 
-import Database.Persist (Entity(Entity, entityVal))
+import Database.Persist
+    ( Entity(Entity, entityVal), PersistUniqueWrite (upsert))
+import qualified Database.Persist as P ((=.))
 import Database.Esqueleto.Experimental
-    (selectOne, from, table, where_, (^.), (==.), val)
+    ( selectOne, from, table, where_, val, update, set
+    , (^.), (==.), (=.)
+    )
 import Data.Text (Text)
 import Data.Text.Encoding (encodeUtf8)
 import Model
     ( UserId, UserPhoto (UserPhoto)
-    , EntityField (UserPhotoUser)
-    , User (User, userName)
+    , EntityField (UserPhotoUser, UserPhotoPhoto, UserPhotoMime, UserName, UserId)
+    , User (User, userName), statusSuccess
     )
 import Foundation
     ( Handler, Widget
     , Route
-      ( HomeR, AccountsR, StaticR, AuthR, AccountPhotoR, AccountEditR, AccountR)
+      ( HomeR, StaticR, AuthR, AccountPhotoR, AccountEditR, AccountR)
     , AppMessage
-      ( MsgUserAccount, MsgBack, MsgCancel
-      , MsgFullName, MsgSignOut, MsgPhoto
-      , MsgSave
+      ( MsgUserAccount, MsgBack, MsgCancel, MsgFullName, MsgSignOut, MsgPhoto
+      , MsgSave, MsgRecordEdited
       )
     )
 import Settings (widgetFile)
 import Settings.StaticFiles
-    ( js_account_min_js, img_person_FILL0_wght400_GRAD0_opsz24_white_svg )
+    ( js_account_min_js, img_person_FILL0_wght400_GRAD0_opsz24_white_svg
+    , img_person_FILL0_wght400_GRAD0_opsz24_svg
+    )
 import Text.Hamlet (Html)
 import Yesod.Auth (Route (LogoutR), maybeAuth)
 import Yesod.Core
     ( Yesod(defaultLayout), SomeMessage (SomeMessage), getMessageRender
-    , MonadHandler (liftHandler), redirect
+    , MonadHandler (liftHandler), redirect, FileInfo (fileContentType)
+    , newIdent, fileSourceByteString, addMessageI
     )
 import Yesod.Core.Content
     (TypedContent (TypedContent), ToContent (toContent))
-import Yesod.Core.Widget (setTitleI, whamlet, addScript)
-import Yesod.Form.Functions (generateFormPost, mopt)
+import Yesod.Core.Widget (setTitleI, addScript)
+import Yesod.Form.Functions (generateFormPost, mopt, runFormPost)
 import Yesod.Form.Types
-    ( MForm, FormResult, FieldView (fvInput)
+    ( MForm, FormResult (FormSuccess), FieldView (fvInput, fvId)
     , FieldSettings (FieldSettings, fsLabel, fsTooltip, fsId, fsName, fsAttrs)
     )
 import Yesod.Persist (YesodPersist(runDB))
 
 import Handler.Material3
     ( md3textField )
+import Yesod.Form.Fields (fileField)
+import Control.Monad (void)
 
 
 postAccountR :: UserId -> Handler Html
-postAccountR uid = undefined
+postAccountR uid = do
+    ((fr,fw),et) <- runFormPost $ formAccount Nothing
+    case fr of
+      FormSuccess (mname,mfi) -> do
+          runDB $ update $ \x -> do
+              set x [ UserName =. val mname ]
+              where_ $ x ^. UserId ==. val uid
+          case mfi of
+            Just fi -> do
+                bs <- fileSourceByteString fi
+                void $ runDB $ upsert (UserPhoto uid (fileContentType fi) bs)
+                    [UserPhotoMime P.=. fileContentType fi, UserPhotoPhoto P.=. bs]
+            Nothing -> return ()
+          addMessageI statusSuccess MsgRecordEdited
+          redirect $ AccountR uid
+      _otherwise -> defaultLayout $ do
+          setTitleI MsgUserAccount
+          $(widgetFile "accounts/edit")
 
 
 getAccountR :: UserId -> Handler Html
@@ -81,20 +106,28 @@ getAccountEditR uid = do
         $(widgetFile "accounts/edit")
 
 
-formAccount :: Maybe (Entity User) -> Html -> MForm Handler (FormResult (Maybe Text), Widget)
+formAccount :: Maybe (Entity User)
+            -> Html -> MForm Handler (FormResult (Maybe Text,Maybe FileInfo), Widget)
 formAccount user extra = do
-    msgRender <- liftHandler getMessageRender
+    rndr <- liftHandler getMessageRender
     (nameR,nameV) <- mopt md3textField FieldSettings
         { fsLabel = SomeMessage MsgFullName
         , fsTooltip = Nothing, fsId = Nothing, fsName = Nothing
-        , fsAttrs = [("label", msgRender MsgFullName)]
+        , fsAttrs = [("label", rndr MsgFullName)]
         } (userName . entityVal <$> user)
-    return ( nameR
-           , [whamlet|
-                     #{extra}
-                     $forall v <- [nameV]
-                       ^{fvInput v}
-                     |]
+    (photoR,photoV) <- mopt fileField FieldSettings
+        { fsLabel = SomeMessage MsgPhoto
+        , fsTooltip = Nothing, fsId = Nothing, fsName = Nothing
+        , fsAttrs = [("style","display:none")]
+        } Nothing
+
+    idUserIdent <- newIdent
+    idLabelPhotoUser <- newIdent
+    idFigurePhotoUser <- newIdent
+    idImgPhotoUser <- newIdent
+
+    return ( (,) <$> nameR <*> photoR
+           , $(widgetFile "accounts/form")
            )
       
 
