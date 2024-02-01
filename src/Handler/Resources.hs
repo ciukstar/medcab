c5b6dfa34325b6693987cd35dd2b55d02366c1fb
ciukstar <ciukstar@yahoo.com>
2024-02-02 00:11:50 +0300

    Add docs for user

--- src/Handler/Resources.hs
+++ src/Handler/Resources.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Handler.Resources (getDocsR) where
 
@@ -12,21 +13,30 @@ import Foundation
       , MsgUserAccount, MsgPhoto, MsgErDiagram, MsgEmail, MsgSuperuser
       , MsgUsername, MsgPassword, MsgClientId, MsgClientSecret, MsgBasicEntities
       , MsgUser, MsgDoctor, MsgSpecialty, MsgSearchEngineOptimization
-      , MsgDoc001, MsgDoc002
+      , MsgSourceCode, MsgIssueTracking
+      , MsgDoc001, MsgDoc002, MsgDoc003, MsgDoc004, MsgDoc005, MsgDoc006
+      , MsgDoc007
       )
     )
 import Model (AvatarColor (AvatarColorLight))
+
 import Settings (widgetFile)
 import Settings.StaticFiles (img_ERD_MedCab_svg)
+
+import Text.Blaze.Html (preEscapedToHtml)
 import Text.Hamlet (Html)
+
 import Yesod.Auth (maybeAuth, Route (LoginR, LogoutR))
-import Yesod.Core (Yesod(defaultLayout), setUltDestCurrent)
+import Yesod.Core
+    ( Yesod(defaultLayout), setUltDestCurrent, getMessageRender, getUrlRender )
 import Yesod.Core.Widget (setTitleI)
 
 
 getDocsR :: Handler Html
 getDocsR = do
     user <- maybeAuth
+    rndr <- getUrlRender
+    translate <- (preEscapedToHtml .) <$> getMessageRender
     defaultLayout $ do
         setUltDestCurrent
         setTitleI MsgDocumentation
